d51d0d8b7f68d91d42935e086c6fc91d08e6490c
Clement Sibille <clements@lisible.xyz>
2024-12-01 09:35:54 +0100

    Add day 01

---
+++ src/01.c
@@ -0,0 +1,107 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LOG(...)                                                               \
+  do {                                                                         \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    fprintf(stderr, "\n");                                                     \
+  } while (0)
+
+#define INPUT_LINE_COUNT 1000
+#define MAX_VALUE 99999
+#define HISTOGRAM_LENGTH (MAX_VALUE + 1)
+#define LINE_BUFFER_CAPACITY 128
+
+int int_cmp(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
+
+bool read_lists_from_file(int left[INPUT_LINE_COUNT],
+                          int right[INPUT_LINE_COUNT],
+                          int right_histogram[HISTOGRAM_LENGTH],
+                          const char *file_path) {
+  FILE *file_handle = fopen(file_path, "r");
+  if (!file_handle) {
+    LOG("Couldn't open file %s: %s", file_path, strerror(errno));
+    goto err;
+  }
+
+  char line[LINE_BUFFER_CAPACITY];
+  int index = 0;
+  ssize_t read;
+  while ((read = fgets(line, LINE_BUFFER_CAPACITY, file_handle) != NULL)) {
+    char *left_value_str = strtok(line, " ");
+    if (!left_value_str) {
+      LOG("Missing left value");
+      goto close_file;
+    }
+
+    char *right_value_str = strtok(NULL, " ");
+    if (!right_value_str) {
+      LOG("Missing right value");
+      goto close_file;
+    }
+
+    left[index] = atoi(left_value_str);
+    right[index] = atoi(right_value_str);
+    right_histogram[right[index]]++;
+
+    index++;
+  }
+  if (!feof(file_handle)) {
+    LOG("An error occured while reading the file");
+    goto close_file;
+  }
+  fclose(file_handle);
+  return true;
+
+close_file:
+  fclose(file_handle);
+err:
+  return false;
+}
+
+int compute_total_distance(int left[INPUT_LINE_COUNT],
+                           int right[INPUT_LINE_COUNT]) {
+  int total_distance = 0;
+  for (int i = 0; i < INPUT_LINE_COUNT; i++) {
+    total_distance += abs(left[i] - right[i]);
+  }
+  return total_distance;
+}
+
+int compute_similarity_score(int left[INPUT_LINE_COUNT],
+                             int right_histogram[HISTOGRAM_LENGTH]) {
+  int similarity_score = 0;
+  for (int i = 0; i < INPUT_LINE_COUNT; i++) {
+    similarity_score += left[i] * right_histogram[left[i]];
+  }
+  return similarity_score;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    LOG("Usage: ./01 <input_file_path>");
+    goto err;
+  }
+
+  int left[INPUT_LINE_COUNT];
+  int right[INPUT_LINE_COUNT];
+  int right_histogram[HISTOGRAM_LENGTH] = {0};
+  if (!read_lists_from_file(left, right, right_histogram, argv[1])) {
+    LOG("Couldn't read list from file: %s", argv[1]);
+    goto err;
+  }
+
+  qsort(left, INPUT_LINE_COUNT, sizeof(int), int_cmp);
+  qsort(right, INPUT_LINE_COUNT, sizeof(int), int_cmp);
+
+  printf("Total distance: %d\n", compute_total_distance(left, right));
+  printf("Similarity score: %d\n",
+         compute_similarity_score(left, right_histogram));
+
+  return 0;
+err:
+  return 1;
+}
